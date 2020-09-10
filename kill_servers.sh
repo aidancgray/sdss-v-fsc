5945fd97dbde6c528b1c5309b36541912d253ec9
Aidan <aidancgray@github.com>
2020-09-10 17:52:11 -0400

    Fixed shell scripts for starting/killing servers

--- kill_servers.sh
+++ kill_servers.sh
@@ -3,10 +3,11 @@
 # system. It will kill: indiserver, image_display.py, trius_cam_server.py,
 # sx_filter_server.py, and stage_server.py.
 
+echo "killing running servers..."
 for pid in $(pgrep -f indiserver); do kill $pid; done
 for pid in $(pgrep -f image_display.py); do kill $pid; done
 for pid in $(pgrep -f trius_cam_server.py); do kill $pid; done
 for pid in $(pgrep -f sx_filter_server.py); do kill $pid; done
 for pid in $(pgrep -f stage_server.py); do kill $pid; done
-
+echo "done"
 			       
--- start_servers.sh
+++ start_servers.sh
@@ -3,7 +3,12 @@
 # the camera control server, filter wheel server, stage server, and
 # image display script.
 
+./kill_servers.sh
+sleep 1
+echo "...starting new servers"
 indiserver indi_sx_ccd indi_sx_wheel &
-nohup ./stage_server.py &
-nohup ./trius_cam_server.py &
-nohup ./sx_filter_server.py &
+sleep 2
+nohup ./stage_server.py >/dev/null 2>&1 &
+nohup ./trius_cam_server.py >/dev/null 2>&1 &
+nohup ./sx_filter_server.py >/dev/null 2>&1 &
+echo "~servers are ready~"
